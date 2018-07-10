d0dfb2570b3e0b9af1c1343f749d4b2cb64a604e
Bogdan5 <bogdan5x@gmail.com>
2018-07-09 22:19:23 -0400

    'Comment'

--- JSFile.js
+++ JSFile.js
@@ -1,4 +1,4 @@
-import * as d3 from 'd3';
+// import * as d3 from 'd3';
 
 let req = new XMLHttpRequest();
 let json = [];
@@ -9,3 +9,20 @@ req.onload = function () {
   json = JSON.parse(req.responseText);
   console.log(json);
 };
+
+let data = json.data;
+const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    svg.selectAll("rect")
+       .data(dataset)
+       .enter()
+       .append("rect")
+       .attr("x", (d, i) => i * 30)
+       .attr("y", (d, i) => h - 3 * d)
+       .attr("width", 25)
+       .attr("height", (d, i) => d * 3)
+       .attr("fill", "navy")
+       .attr("class", "bar")

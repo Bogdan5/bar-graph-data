3f7ad0bc20d806e243f2860c21f3933ba7aeaada
Bogdan5 <bogdan5x@gmail.com>
2018-07-10 19:33:10 -0400

    Comment

--- JSFile.js
+++ JSFile.js
@@ -1,28 +1,32 @@
 // import * as d3 from 'd3';
 
 let req = new XMLHttpRequest();
-let json = [];
+let jsonData = [];
 req.open('GET',
 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true);
 req.send();
 req.onload = function () {
-  json = JSON.parse(req.responseText);
-  console.log(json);
+  jsonData = JSON.parse(req.responseText);
+  graphDraw(jsonData);
 };
 
-let data = json.data;
-const svg = d3.select("body")
-                  .append("svg")
-                  .attr("width", w)
-                  .attr("height", h);
+const graphDraw = (data) => {
+  console.log(data.data);
+  let w = 1500;
+  let h = 1800;
+  const svg = d3.select('body')
+                    .append('svg')
+                    .attr('width', w)
+                    .attr('height', h);
 
-    svg.selectAll("rect")
-       .data(dataset)
-       .enter()
-       .append("rect")
-       .attr("x", (d, i) => i * 30)
-       .attr("y", (d, i) => h - 3 * d)
-       .attr("width", 25)
-       .attr("height", (d, i) => d * 3)
-       .attr("fill", "navy")
-       .attr("class", "bar")
+  svg.selectAll('rect')
+         .data(data.data)
+         .enter()
+         .append('rect')
+         .attr('x', (d, i) => i * 10)
+         .attr('y', (d, i) => h -  d[1])
+         .attr('width', 8)
+         .attr('height', (d, i) => d[1])
+         .attr('fill', 'navy')
+         .attr('class', 'bar');
+};

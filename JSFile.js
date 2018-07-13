d9ba26f2554c9a91ac1594231cc64718e8672cd3
Bogdan5 <bogdan5x@gmail.com>
2018-07-13 19:24:58 -0400

    Comment

--- JSFile.js
+++ JSFile.js
@@ -12,35 +12,30 @@ req.onload = function () {
 
 const graphDraw = (data) => {
   console.log('graphDraw', data);
-  let w = 1200;
-  let h = 600;
+  let w = 1140;
+  let h = 500;
   const padding = 40;
-  let xData = data.reduce((acc, item, index) => {
-    if ((index - 12) % 20 === 0) {
-      return acc.concat(item[0].substring(0, 4));
+  let yearRange = [];
+  let yearData = data.reduce((acc, item, index) => {
+    let x = parseInt(item[0].substring(0, 4));
+    if (item[0].substring(5, 7) === '01' && x % 5 === 0) {
+      yearRange.concat(index * 10);
+      return acc.concat(x);
     } else {
       return acc;
     }
   }, []);
-  let xRange = data.reduce((acc, item, index) => {
-    if ((index - 12) % 20 === 0) {
-      return acc.concat(index * 4);
-    } else {
-      return acc;
-    }
-  }, []);
-  console.log('xRange', xRange);
-
-  const xScale = d3.scaleOrdinal();
+  console.log('yearData', yearData);
+  const xScale = d3.scalePoint();
   const yScale = d3.scaleLinear();
 
   xScale
-    .domain(xData)
-    .range(xRange);
+    .domain(yearData)
+    .range([0, 1100]);
 
   yScale
     .domain([d3.min(data, (d) => d[1]), d3.max(data, (d) => d[1])])
-    .range([h - padding, padding]);
+    .range([h, 0]);
 
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale);
@@ -55,9 +50,9 @@ const graphDraw = (data) => {
          .enter()
          .append('rect')
          .attr('x', (d, i) => i * 4 + padding)
-         .attr('y', (d, i) => yScale(d[1] + padding))
+         .attr('y', (d, i) => h - yScale(d[1] + padding))
          .attr('width', 3)
-         .attr('height', (d, i) => h - yScale(d[1]) - padding)
+         .attr('height', (d, i) => yScale(d[1]))
          .attr('fill', 'navy')
          .attr('class', 'bar')
          .on('mouseover', (d) => {

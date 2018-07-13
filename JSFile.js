313fb0fb270ca3154d4e9e20ad2b359f88d5fd92
Bogdan5 <bogdan5x@gmail.com>
2018-07-12 22:02:03 -0400

    "Comment"

--- JSFile.js
+++ JSFile.js
@@ -12,15 +12,38 @@ req.onload = function () {
 
 const graphDraw = (data) => {
   console.log('graphDraw', data);
-  let w = 600;
+  let w = 1200;
   let h = 600;
+  const padding = 40;
+  let xData = data.reduce((acc, item, index) => {
+    if (index % 20 === 0) {
+      return acc.concat(item[0].substring(0, 4));
+    } else {
+      return acc;
+    }
+  }, []);
+  let xRange = data.reduce((acc, item, index) => {
+    if (index % 20 === 0) {
+      return acc.concat(index * 4);
+    } else {
+      return acc;
+    }
+  }, []);
+  console.log('xRange', xRange);
 
-  const myScale = d3.scaleLinear();
-  myScale
-    .domain([d3.min(data, (d) => d[1]), d3.max(data, (d) => d[1])])
-    .range([30, w - 30]);
+  const xScale = d3.scaleOrdinal();
+  const yScale = d3.scaleLinear();
 
-  console.log('scale', myScale(10000));
+  xScale
+    .domain(xData)
+    .range(xRange);
+
+  yScale
+    .domain([d3.max(data, (d) => d[1]), d3.min(data, (d) => d[1])])
+    .range([padding, h - padding]);
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
 
   const svg = d3.select('body')
                     .append('svg')
@@ -31,10 +54,30 @@ const graphDraw = (data) => {
          .data(data)
          .enter()
          .append('rect')
-         .attr('x', (d, i) => i * 4)
-         .attr('y', (d, i) => h - myScale(d[1]))
-         .attr('width', 2)
-         .attr('height', (d, i) => myScale(d[1]))
+         .attr('x', (d, i) => i * 4 + padding)
+         .attr('y', (d, i) => h - yScale(d[1]) - padding)
+         .attr('width', 3)
+         .attr('height', (d, i) => yScale(d[1]))
          .attr('fill', 'navy')
-         .attr('class', 'bar');
+         .attr('class', 'bar')
+         .on('mouseover', () => {
+           tooltip.style('visibility', 'visible');
+           tooltip.text(d[0] + )
+         )
+         .on('mouseleave', () => tooltip.style('visibility', 'hidden'));
+
+  let tooltip = d3.select('body')
+    .append('div')
+    .style('position', 'absolute')
+    .style('z-index', '10')
+    .style('visibility', 'hidden')
+    .text('a simple tooltip');
+
+  svg.append('g')
+    .attr('transform', 'translate(0,' + (h - padding) + ')')
+    .call(xAxis);
+
+  svg.append('g')
+    .attr('transform', 'translate(' + padding + ', 0)')
+    .call(yAxis);
 };

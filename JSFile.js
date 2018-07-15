7c26864df85010312481e161a45007cd780d6d76
Bogdan5 <bogdan5x@gmail.com>
2018-07-14 20:18:55 -0400

    Comment

--- JSFile.js
+++ JSFile.js
@@ -15,27 +15,19 @@ const graphDraw = (data) => {
   let w = 1140;
   let h = 500;
   const padding = 40;
-  let parser = d3.timeParse('%Y-%M-%D');
-  let yearData = data.reduce((acc, item) => [parser(item[0]), item[1]], []);
+  let parser = d3.timeParse('%Y-%m-%d');
+  let yearData = data.reduce((acc, item) => acc.concat([[parser(item[0]), item[1]]]), []);
 
-  data.reduce((acc, item, index) => {
-    if (item[0].substring(5, 7) === '01') {
-      return acc.concat(parseInt(item[0].substring(0, 4)));
-    } else {
-      return acc;
-    }
-  }, []);
-  console.log('yearData', yearData);
-  const xScale = d3.scalePoint();
+  const xScale = d3.scaleTime();
   const yScale = d3.scaleLinear();
 
   xScale
-    .domain(yearData)
-    .range([0, 1100]);
+    .domain([yearData[0][0], yearData[data.length - 1][0]])
+    .range([0, w - padding]);
 
   yScale
-    .domain([d3.min(data, (d) => d[1]), d3.max(data, (d) => d[1])])
-    .range([h, 0]);
+    .domain([0, d3.max(data, (d) => d[1])])
+    .range([h - padding, 0]);
 
   const xAxis = d3.axisBottom(xScale);
   const yAxis = d3.axisLeft(yScale);
@@ -43,16 +35,17 @@ const graphDraw = (data) => {
   const svg = d3.select('body')
                     .append('svg')
                     .attr('width', w)
-                    .attr('height', h);
+                    .attr('height', h)
+                    .attr('transform', 'translate(${padding}), ${0}');
 
   svg.selectAll('rect')
-         .data(data)
+         .data(yearData)
          .enter()
          .append('rect')
-         .attr('x', (d, i) => i * 4 + padding)
+         .attr('x', (d) => xScale(d[0]) + padding)
          .attr('y', (d, i) => yScale(d[1] + padding))
          .attr('width', 3)
-         .attr('height', (d, i) => h - yScale(d[1]))
+         .attr('height', (d, i) => h - yScale(d[1]) - padding)
          .attr('fill', 'navy')
          .attr('class', 'bar')
          .on('mouseover', (d) => {
@@ -69,7 +62,7 @@ const graphDraw = (data) => {
     .text('a simple tooltip');
 
   svg.append('g')
-    .attr('transform', 'translate(' + padding + ', ' + (h - padding) + ')')
+    // .attr('transform', 'translate(' + 0 + ', ' + h - 50 + ')')
     .call(xAxis);
 
   svg.append('g')
